db071dbe3ad55c1761a0d61cd1c5c541991c3bab
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-01-06 21:27:36 +0200

    twrp_y2s.mk -> pbrp_y2s.mk

---
+++ pbrp_y2s.mk
@@ -0,0 +1,23 @@
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common PBRP stuff
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/y2s/device.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/y2s/recovery/root,recovery/root)
+
+# Device identifier
+PRODUCT_RELEASE_NAME := y2s
+PRODUCT_DEVICE := y2s
+PRODUCT_NAME := twrp_y2s
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy S20+
+PRODUCT_MANUFACTURER := samsung
+
